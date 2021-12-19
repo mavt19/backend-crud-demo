c09fffb00b5a3cfc58ffd34fd4837a251962a171
Moises Vera <mavt10rm@gmail.com>
2021-12-19 11:17:11 -0500

    first commit

---
+++ src/main/java/com/backend/BackendCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrudDemoApplication.class, args);
+	}
+
+}
