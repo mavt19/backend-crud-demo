a5949d34e7f3099d713e8bca9d62e405eec19bf1
Moises Vera <mavt10rm@gmail.com>
2022-01-02 12:26:22 -0500

    add swagger

--- src/main/java/com/backend/controller/TodoController.java
+++ src/main/java/com/backend/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.backend.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class TodoController {
 		Todo todoSave = todoService.create(todo);
 		return  ResponseEntity.ok(todoSave);
 	}
-	@PutMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	private ResponseEntity<?> delete(@PathVariable("id") Long id){
 		Todo todoDelete = todoService.get(id);
 		if(todoDelete == null) {
